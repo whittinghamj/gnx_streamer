accdbff1fadce0afc61da8aa4eba9fd5663b4c38
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 11:17:19 +0000

    no message

--- test.php
+++ test.php
@@ -1,5 +1,13 @@
 <?php
 
 if ( extension_loaded('pdo') ) {
-    echo "PDO installed \n";
+    echo "PDO installed. \n";
+}else{
+	echo "PDO is NOT installed. \n";
+}
+
+if ( extension_loaded('pdo_mysql') ) { // e.g., pdo_mysql
+    echo "PDO MySQL installed. \n";
+}else{
+    echo "PDO MySQL in NOT installed. \n";
 }
