bf0ea2ced580e423db0e9c27492cb1811271c143
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 22:35:09 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -428,40 +428,12 @@ if($_SESSION['logged_in'] != true) {
 													</select>
 												</div>
 											</div>
-										</div>
-										<div class="isotope-item document col-sm-6 col-md-4 col-lg-3">
-											<div class="thumbnail">
-												<div class="thumb-preview">
-													<a class="thumb-image" href="assets/images/projects/project-1.jpg">
-														<img src="assets/images/projects/project-1.jpg" class="img-responsive" alt="Project">
-													</a>
-													<div class="mg-thumb-options">
-														<div class="mg-zoom"><i class="fa fa-search"></i></div>
-														<div class="mg-toolbar">
-															<div class="mg-option checkbox-custom checkbox-inline">
-																<input type="checkbox" id="file_1" value="1">
-																<label for="file_1">SELECT</label>
-															</div>
-															<div class="mg-group pull-right">
-																<a href="#">EDIT</a>
-																<button class="dropdown-toggle mg-toggle" type="button" data-toggle="dropdown">
-																	<i class="fa fa-caret-up"></i>
-																</button>
-																<ul class="dropdown-menu mg-menu" role="menu">
-																	<li><a href="#"><i class="fa fa-download"></i> Download</a></li>
-																	<li><a href="#"><i class="fa fa-trash-o"></i> Delete</a></li>
-																</ul>
-															</div>
-														</div>
-													</div>
-												</div>
-												<h5 class="mg-title text-semibold">SEO<small>.png</small></h5>
-												<div class="mg-description">
-													<small class="pull-left text-muted">Design, Websites</small>
-													<small class="pull-right text-muted">07/10/2014</small>
-												</div>
+
+											<div class="form-group col-md-3">
+												<img src="screenshots/video0.png" alt="" height="320" width="240">
 											</div>
 										</div>
+
 										<footer class="panel-footer">
 											<button type="submit" class="btn btn-success">Submit </button>
 										</footer>
