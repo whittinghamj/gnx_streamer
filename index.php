72bf8eb1c73a7badc35a9fb9dc268e37d23fb29f
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 11:19:17 +0000

    no message

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+include('inc/global_vars.php');
+
+?>
 <!doctype html>
 <html class="fixed">
 	<head>
