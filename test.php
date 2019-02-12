1457c78eb3b8464ee575b0ce1ba7d3c0262113dd
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 16:21:43 +0000

    no message

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
 $source['name']				= 'video0';
-$source['raw_json']			= file_get_contents("actions.php?a=source_check&source=".$source['name']);
+$source['raw_json']			= file_get_contents("/actions.php?a=source_check&source=".$source['name']);
 
 echo '<pre>';
 print_r($source);
