eac5e1d93a012e6ecdeffb76c6573e21f7e99c6b
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 11:27:53 +0000

    no message

--- inc/db.php
+++ inc/db.php
@@ -1,33 +1,16 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "Mimi!#&@";
 
-// MySQL Connection information variables.
-$db 			= "";
-$hostname 		= 'localhost';
-$username 		= 'root';
-$password 		= 'Mimi\!\#\&\@';
-$database 		= 'streamer';
-
-try
-{ // Create connection to MySQL Database.
-	$db = new PDO
-		( // Check connection to db MySQL Database.
-		"mysql:host=$hostname;dbname=$database",
-		$username,
-		$password
-		);
-	$db->setAttribute
-		( // Prepare Attribute for Error Mode Exception Handling.
-		PDO::ATTR_ERRMODE,
-		PDO::ERRMODE_EXCEPTION
-		);
-}
-catch(PDOException $_e)
-{ // If error occurs, Exception causes automatic transaction rollback...
-	$db = null; // Null connection to the database...
-//	mail_errors($_e->getMessage()); // Mail errors accordingly.
-	print_r ($_e->getMessage());
-}
-
-// $db = null; // Null connection variable closes connection.
-
-?>
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=streamer", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
