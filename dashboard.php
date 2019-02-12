f42cda7b298480ef0ffd2606b03cb28454f1b49b
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 18:52:00 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -418,8 +418,8 @@ if($_SESSION['logged_in'] != true) {
 					type: "GET",
 			        url:'actions.php?a=ajax_source_list',
 					success: function(sources) {
-						// var x = sources[0].source.name;
-						// console.log('Source Name: ' + x);
+						var x = sources[0].source.name;
+						console.log('Source Name: ' + x);
 
 						for (i in sources)
 						{
