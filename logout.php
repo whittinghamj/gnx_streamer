08333f8cc854cc34b2725bca47f4bf301790fbb5
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 22:45:07 +0000

    no message

--- logout.php
+++ logout.php
@@ -1,13 +1,6 @@
 <?php
-
-include('inc/db.php');
-include('inc/sessions.php');
-$sess = new SessionManager();
 session_start();
-
-include('inc/global_vars.php');
-include('inc/functions.php');
 
 session_destroy();
 
-go($site['url'].'/index');
+go($site['url'].'/index.php');
