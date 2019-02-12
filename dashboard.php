72f7087c8efff55d02dea6c43a4b3e78ce479ffa
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 15:31:18 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -545,38 +545,198 @@ if($_SESSION['logged_in'] != true) {
 				</aside>
 				<!-- end: sidebar -->
 
-				<section role="main" class="content-body">
-					<header class="page-header">
-						<h2>Dashboard</h2>
+				<?php
+					$c = $_GET['c'];
+					switch ($c){
+						// test
+						case "test":
+							test();
+							break;
 
-						<div class="right-wrapper pull-right">
-							<ol class="breadcrumbs">
-								<li>
-									<a href="index.html">
-										<i class="fa fa-home"></i>
-									</a>
-								</li>
-								<!--
-								<li><span>Layouts</span></li>
-								<li><span>Boxed</span></li>
-								-->
-							</ol>
-					
-							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
-						</div>
-					</header>
+		                // sources
+		                case "sources":
+		                    sources();
+		                    break;
 
-					<!-- start: page -->
+		                // source
+		                case "source":
+		                    source();
+		                    break;
+							
+						// home
+						default:
+							home();
+							break;
+					}
+				?>
 
-					<section class="panel">
-						<div class="panel-body">
-							content goes here
-						</div>
+				<?php function home() { ?>
+					<section role="main" class="content-body">
+						<header class="page-header">
+							<h2>Dashboard</h2>
+
+							<div class="right-wrapper pull-right">
+								<ol class="breadcrumbs">
+									<li>
+										<a href="dashboard.php">
+											<i class="fa fa-home"></i>
+										</a>
+									</li>
+									<!--
+									<li><span>Layouts</span></li>
+									<li><span>Boxed</span></li>
+									-->
+								</ol>
+						
+								<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+							</div>
+						</header>
+
+						<!-- start: page -->
+
+						<section class="panel">
+							<div class="panel-body">
+								dashboard home content
+							</div>
+						</section>
+
+						<!-- end: page -->
+
 					</section>
+				<?php } ?>
 
-					<!-- end: page -->
+				<?php function sources() { ?>
+					<section role="main" class="content-body">
+						<header class="page-header">
+							<h2>Capture Sources</h2>
 
-				</section>
+							<div class="right-wrapper pull-right">
+								<ol class="breadcrumbs">
+									<li>
+										<a href="dashboard.php">
+											<i class="fa fa-home"></i>
+										</a>
+									</li>
+									<li><span>Sources</span></li>
+									<!--
+									<li><span>Boxed</span></li>
+									-->
+								</ol>
+						
+								<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+							</div>
+						</header>
+
+						<!-- start: page -->
+
+						<section class="panel">
+							<div class="panel-body">
+								<div class="table-responsive">
+									<table class="table table-striped mb-none">
+										<thead>
+											<tr>
+												<th>#</th>
+												<th>Project</th>
+												<th>Status</th>
+												<th>Progress</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>1</td>
+												<td>JSOFT - Responsive HTML5 Template</td>
+												<td><span class="label label-success">Success</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded m-none mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+															100%
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>2</td>
+												<td>JSOFT - Responsive Drupal 7 Theme</td>
+												<td><span class="label label-success">Success</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded m-none mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+															100%
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>3</td>
+												<td>Tucson - Responsive HTML5 Template</td>
+												<td><span class="label label-warning">Warning</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded m-none mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+															60%
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>4</td>
+												<td>Tucson - Responsive Business WordPress Theme</td>
+												<td><span class="label label-success">Success</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded m-none mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
+															90%
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>5</td>
+												<td>JSOFT - Responsive Admin HTML5 Template</td>
+												<td><span class="label label-warning">Warning</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded m-none mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 45%;">
+															45%
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>6</td>
+												<td>JSOFT - Responsive HTML5 Template</td>
+												<td><span class="label label-danger">Danger</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded m-none mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+															40%
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>7</td>
+												<td>JSOFT - Responsive Drupal 7 Theme</td>
+												<td><span class="label label-success">Success</span></td>
+												<td>
+													<div class="progress progress-sm progress-half-rounded mt-xs light">
+														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 95%;">
+															95%
+														</div>
+													</div>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</section>
+
+						<!-- end: page -->
+
+					</section>
+				<?php } ?>
+
 			</div>
 
 			<aside id="sidebar-right" class="sidebar-right">
