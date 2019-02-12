42d47b31c2310144f3498f71e53415b602379115
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 23:19:14 +0000

    no message

--- console/console.php
+++ console/console.php
@@ -14,5 +14,13 @@ if($task == 'test')
 		$data = json_decode($config_file);
 
 		print_r($data);
+
+		$cmd = "ffmpeg -y -f alsa -ac 2 -i hw:1,0 -f video4linux2 -re -framerate 29 -i /dev/video0 -acodec aac -ab 128k -ar 44100 -f matroska -vcodec libx264 -r 30 -pix_fmt yuv420p -s 1280x720 -preset ultrafast -b:v 4000k -f flv rtmp://iptv.genexnetworks.net:25462/live/sky_cinema_comedy_hd -f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png";
+
+		echo "==================================================\n";
+		echo $cmd;
+		echo "==================================================\n";
+
+		echo "\n";
 	}
 }
