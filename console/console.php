d9cdbd1c514c0634d83fa4b049519e828d8e8074
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 23:23:56 +0000

    no message

--- console/console.php
+++ console/console.php
@@ -12,11 +12,11 @@ if($task == 'test')
 
 	foreach($config_files as $config_file) {
 		$file = file_get_contents($config_file);
-		$data = json_decode($file);
+		$data = json_decode($file, true);
 
 		print_r($data);
 
-		$cmd = "ffmpeg -y -f alsa -ac 2 -i hw:1,0 -f video4linux2 -re -framerate 29 -i /dev/video0 -acodec aac -ab 128k -ar 44100 -f matroska -vcodec libx264 -r 30 -pix_fmt yuv420p -s 1280x720 -preset ultrafast -b:v 4000k -f flv rtmp://iptv.genexnetworks.net:25462/live/sky_cinema_comedy_hd -f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png";
+		$cmd = "ffmpeg -y -f alsa -ac 2 -i hw:1,0 -f video4linux2 -re -framerate 29 -i /dev/".$data['source']." -acodec aac -ab 128k -ar 44100 -f matroska -vcodec ".$data['codec']." -r ".$data['framerate']." -pix_fmt yuv420p -s ".$data['screen_resolution']." -preset ultrafast -b:v ".$data['bitrate']." -f flv ".$data['rtmp_server']." -f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png";
 
 		echo "====================================================================================================\n";
 		echo $cmd;
