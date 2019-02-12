75c6b3ff38fe39916b69ccf9aa0b4b0c759142a8
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 11:10:36 +0000

    no message

--- inc/db.php
+++ inc/db.php
@@ -6,16 +6,4 @@ $user = 'root';
 $pass = 'Mimi!#&@';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-	$pdo = new PDO($dsn, $user, $pass, $options);
-    echo "MySQL Connected.";
-} catch (\PDOException $e) {
-	echo "MySQL Error. check log files.";
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+$db = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8mb4', $user, $pass);
