2c44425b399a29818fba5f74a4bc4f375d566f47
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-12 18:49:32 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -423,8 +423,8 @@ if($_SESSION['logged_in'] != true) {
 
 						for (i in sources)
 						{
-							// colum 0
-							document.getElementById(sources[i].source.name + '_col_2').innerHTML = sources[i].source.name;
+							// colum 2
+							document.getElementById(sources[i].source.name + '_col_2').innerHTML = 'test' + sources[i];
 						}						
 					}
 				});
