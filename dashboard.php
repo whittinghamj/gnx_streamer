38497625edd8374894e0d5f13517031ea5b252ef
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-13 11:49:21 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -618,7 +618,7 @@ if($_SESSION['logged_in'] != true) {
 								if(sources[i].source.status == 'busy') {
 									document.getElementById(sources[i].source.name + '_col_4').innerHTML = '<button class="btn btn-danger btn-flat"><i class="fa fa-times"></i></button> <a title="Edit" class="btn btn-info btn-flat" href="dashboard.php?c=source&source=' + sources[i].source.name + '"><i class="fa fa-globe"></i></a>';
 								} else {
-									document.getElementById(sources[i].source.name + '_col_4').innerHTML = '<buttonclass="btn btn-success btn-flat"><i class="fa fa-check"></i></button> <a title="Edit" class="btn btn-info btn-flat" href="dashboard.php?c=source&source=' + sources[i].source.name + '"><i class="fa fa-globe"></i></a>';
+									document.getElementById(sources[i].source.name + '_col_4').innerHTML = '<button class="btn btn-success btn-flat"><i class="fa fa-check"></i></button> <a title="Edit" class="btn btn-info btn-flat" href="dashboard.php?c=source&source=' + sources[i].source.name + '"><i class="fa fa-globe"></i></a>';
 								}
 							}						
 						}
