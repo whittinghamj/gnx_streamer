0c46cd93b7d10307acc7d0803173b1869dc35375
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-13 15:37:18 +0000

    no message

--- console/console.php
+++ console/console.php
@@ -14,15 +14,15 @@ if($task == 'test')
 		$file = file_get_contents($config_file);
 		$data = json_decode($file, true);
 
-		print_r($data);
+		// print_r($data);
 
 		$cmd = "ffmpeg -nostdin -loglevel fatal -y -f alsa -ac 2 -i hw:1,0 -f video4linux2 -re -framerate ".$data['framerate_in']." -i /dev/".$data['source']." -acodec aac -ab 128k -ar 44100 -f matroska -vcodec ".$data['video_codec']." -r ".$data['framerate_out']." -pix_fmt yuv420p -s ".$data['screen_resolution']." -preset ultrafast -b:v ".$data['bitrate']."k -f flv ".$data['rtmp_server']." ".($data['screenshot']=='enable' ? '-f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png' : '');
 
-		echo "====================================================================================================\n";
-		echo $cmd . "\n";
-		echo "====================================================================================================\n";
+		// echo "====================================================================================================\n";
+		// echo $cmd . "\n";
+		// echo "====================================================================================================\n";
 
-		echo "\n";
+		// echo "\n";
 
 		$pid = exec("ps aux | grep '/dev/".$data['source']."' | grep -v 'grep' | grep -v '0:00' | awk '{print $2}'");
 
