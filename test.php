87bd1cec45a3f2896e81e6f6c3f96f667bae9b45
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-14 01:15:54 +0000

    no message

--- test.php
+++ test.php
@@ -3,7 +3,13 @@
 $audio_devices 			= shell_exec('arecord -L | grep "hw:CARD=SB" | grep -v "plug"');
 
 
-echo "printing results \n";
+echo "printing raw results \n";
 echo $audio_devices;
+
+echo "\n\n";
+
+echo "printing array results \n";
+$array = explode("\n", $audio_devices);
+print_r($array);
 
 echo "\n\n";
