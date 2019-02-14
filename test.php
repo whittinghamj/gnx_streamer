46888bb3607f2cb966409b8932d9821539fbd061
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-14 01:11:30 +0000

    no message

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php
 
-$audio_devices 			= passthru('arecord -L | grep "hw:CARD=SB" | grep -v "plug"');
+$audio_devices 			= shell_exec('arecord -L | grep "hw:CARD=SB" | grep -v "plug"');
+
+
+echo "printing results \n";
+echo $audio_devices;
 
 echo "\n\n";
