33d0ed99f7335d75980248bbbda8c4411be1c9cd
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-14 15:59:07 +0000

    no message

--- console/console.php
+++ console/console.php
@@ -17,12 +17,20 @@ if($task == 'stop_start')
 
 		// print_r($data);
 
-		$cmd = "ffmpeg -nostdin -loglevel fatal -y -f alsa -ac 2 -i ".$data['audio_device']." -f video4linux2 -re -framerate ".$data['framerate_in']." -i /dev/".$data['source']." -acodec aac -ab 128k -ar 44100 -f matroska -vcodec ".$data['video_codec']." -r ".$data['framerate_out']." -pix_fmt yuv420p -s ".$data['screen_resolution']." -preset ultrafast -b:v ".$data['bitrate']."k -f flv ".$data['rtmp_server']." ".($data['screenshot']=='enable' ? '-f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png' : '');
+		if($data['out_type'] == 'rtmp') {
+			$output_url = $data['rtmp_server'];
+		}elseif($data['out_type'] == 'http'){
+			$output_url = 'rtmp://localhost/show/stream';
+		}
+
+		$cmd = "ffmpeg -nostdin -loglevel fatal -y -f alsa -ac 2 -i ".$data['audio_device']." -f video4linux2 -re -framerate ".$data['framerate_in']." -i /dev/".$data['source']." -acodec aac -ab 128k -ar 44100 -f matroska -vcodec ".$data['video_codec']." -r ".$data['framerate_out']." -pix_fmt yuv420p -s ".$data['screen_resolution']." -preset ultrafast -b:v ".$data['bitrate']."k -f flv ".$output_url." ".($data['screenshot']=='enable' ? '-f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png' : '');
 
 		echo "====================================================================================================\n";
 		echo $cmd . "\n";
 		echo "====================================================================================================\n";
 		echo "\n";
+
+		echo "Output URL: " . $output_url . "" \n;
 
 		$pid = exec("ps aux | grep '/dev/".$data['source']."' | grep -v 'grep' | grep -v '0:00' | awk '{print $2}'");
 
