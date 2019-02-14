7563fec4d0537828d6693645c40a8dc225e5ccd6
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-14 03:22:35 +0000

    no message

--- test.php
+++ test.php
@@ -1,21 +1,6 @@
 <?php
 
 $audio_devices 			= shell_exec('arecord -L | grep "hw:CARD=SB" | grep -v "plug"');
-
-
-echo "printing raw results \n";
-echo $audio_devices;
-
-echo "\n\n";
-
-echo "printing array results \n";
-$array = explode("\n", $audio_devices);
-print_r($array);
-
-echo "\n\n";
-
-echo "printing filteresarray results \n";
-$result = array_filter($array);
+$array 					= explode("\n", $audio_devices);
+$result 				= array_filter($array);
 print_r($result);
-
-echo "\n";       
