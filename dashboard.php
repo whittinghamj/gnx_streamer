02c55909a2da4218df6cf1bf04c7669413379594
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-15 23:34:03 +0000

    no message

--- dashboard.php
+++ dashboard.php
@@ -286,6 +286,31 @@ if($_SESSION['logged_in'] != true) {
 											</table>
 										</div>
 									</div>
+
+									<a class="mb-xs mt-xs mr-xs modal-sizes btn btn-default" href="#modalFull">Full</a>
+
+									<div id="modalFull" class="modal-block modal-block-full mfp-hide">
+										<section class="panel">
+											<header class="panel-heading">
+												<h2 class="panel-title">Are you sure?</h2>
+											</header>
+											<div class="panel-body">
+												<div class="modal-wrapper">
+													<div class="modal-text">
+														<p>Are you sure that you want to delete this image?</p>
+													</div>
+												</div>
+											</div>
+											<footer class="panel-footer">
+												<div class="row">
+													<div class="col-md-12 text-right">
+														<button class="btn btn-primary modal-confirm">Confirm</button>
+														<button class="btn btn-default modal-dismiss">Cancel</button>
+													</div>
+												</div>
+											</footer>
+										</section>
+									</div>
 								</section>
 							</div>
 						</div>
@@ -801,5 +826,7 @@ if($_SESSION['logged_in'] != true) {
 				}
 			}
 		</script>
+
+		<script src="assets/javascripts/ui-elements/examples.modals.js"></script>
 	</body>
 </html>
