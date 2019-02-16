87e3d8b329f7408d5a71d04fe867cba3b6fe9af5
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-16 00:46:54 +0000

    no message

--- test.php
+++ test.php
@@ -1,68 +1,34 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script>
+  $(function() {
+    $(".video").click(function () {
+      var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+      $(theModal + ' iframe').attr('src', videoSRCauto);
+      $(theModal + ' button.close').click(function () {
+        $(theModal + ' iframe').attr('src', videoSRC);
+      });
+    });
+  });
+  </script>
+</head>
 <body>
-<section class="text-center">
-  <h1>click on image below</h1>
-<img data-toggle="modal" data-target="#homeVideo" class="img-responsive" src="http://www.gossettmktg.com/video/dangot.png" onclick="playVid()" />
-</section>
-</body>
-
-<style>
-body {
-    background-color: #30b3f2;
-}
-h1 {
-  color: #fff;
-}
-section {
-    padding-top: 2em;
-    padding-bottom: 2em;
-}
-section h1 {
-  margin-bottom: 1em;
-}
-section img {
-  border: #ddd solid 1px;
-  border-radius: 5px;
-}
-#homeVideo button.btn.btn-default {
-    background: black;
-    border-radius: 50%;
-    position: absolute;
-    right: 0;
-    z-index: 5;
-    color: white;
-}
-</style>
-
-<!-- Home Video Modal -->
-<div class="modal fade" id="homeVideo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <button type="button" class="btn btn-default" data-dismiss="modal" onclick="pauseVid()">X</button>
-      <div class="embed-responsive embed-responsive-16by9">
-        <video id="gossVideo" class="embed-responsive-item" controls="controls" poster="http://www.gossettmktg.com/video/dangot.png">
-          <source src="http://www.gossettmktg.com/video/dangot.mp4" type="video/mp4">
-          <source src="http://www.gossettmktg.com/video/dangot.webm" type="video/webm">
-          <source src="http://www.gossettmktg.com/video/dangot.ogv" type="video/ogg">
-          <object type="application/x-shockwave-flash" data="https://releases.flowplayer.org/swf/flowplayer-3.2.1.swf" width="353" height="190">
-            <param name="movie" value="https://releases.flowplayer.org/swf/flowplayer-3.2.1.swf">
-            <param name="allowFullScreen" value="true">		<param name="wmode" value="transparent">
-            <param name="flashVars" value="config={'playlist':['http%3A%2F%2Fwww.gossettmktg.com%2Fvideo%2Fdangot.png',{'url':'http%3A%2F%2Fwww.gossettmktg.com%2Fvideo%2Fdangot.mp4','autoPlay':false}]}">
-            <img alt="Big Buck Bunny" src="http://www.gossettmktg.com/video/dangot.png" width="353" height="190" title="No video playback capabilities, please download the video below">
-          </object>
-        </video>
+  <button class="btn btn-success btn-lg video" data-video="https://www.youtube.com/embed/8L5PqL4ylIU" data-toggle="modal" data-target="#videoModal">Play Video 1</button>
+  <button class="btn btn-default btn-lg video" data-video="https://www.youtube.com/embed/HBdcL7YsdBY" data-toggle="modal" data-target="#videoModal">Play Video 2</button>
+  <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-body">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <iframe width="100%" height="350" src="" frameborder="0" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
   </div>
-</div>
-
-<script>
-	var vid = document.getElementById("gossVideo"); 
-
-function playVid() { 
-    vid.play(); 
-} 
-
-function pauseVid() { 
-    vid.pause(); 
-} 
-</script>
+</body>
+</html>
