7c2dc8b8b7ff1e357224d32f73c4455103f6deb8
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-17 12:53:45 +0000

    no message

--- console/console.php
+++ console/console.php
@@ -22,7 +22,13 @@ if($task == 'stop_start') {
 			$output_url = 'rtmp://localhost/show/' . $data['source'];
 		}
 
-		$cmd = "ffmpeg -nostdin -loglevel fatal -y -f alsa -ac 2 -i ".$data['audio_device']." -f video4linux2 -re -framerate ".$data['framerate_in']." -i /dev/".$data['source']." -acodec aac -ab 128k -ar 44100 -f matroska -vcodec ".$data['video_codec']." -r ".$data['framerate_out']." -pix_fmt yuv420p -s ".$data['screen_resolution']." -preset ultrafast -b:v ".$data['bitrate']."k -f flv ".$output_url." ".($data['screenshot']=='enable' ? '-f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png' : '');
+		if($data['watermark_type'] != 'disable') {
+			$watermark = '';
+		}else{
+			$watermark = '-i '.$data['watermark_image_url'].' -filter_complex "overlay=10:10" ';
+		}
+
+		$cmd = "ffmpeg -nostdin -loglevel fatal -y -f alsa -ac 2 -i ".$data['audio_device']." -f video4linux2 -re -framerate ".$data['framerate_in']." -i /dev/".$data['source']." ".$watermark." -acodec aac -ab 128k -ar 44100 -f matroska -vcodec ".$data['video_codec']." -r ".$data['framerate_out']." -pix_fmt yuv420p -s ".$data['screen_resolution']." -preset ultrafast -b:v ".$data['bitrate']."k -f flv ".$output_url." ".($data['screenshot']=='enable' ? '-f image2 -vf fps=fps=90 -s 320x240 -updatefirst 1 /var/www/html/screenshots/video0.png' : '');
 
 		echo "====================================================================================================\n";
 		echo $cmd . "\n";
