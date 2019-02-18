d78cc3de80ebe58295ea219515f931c40473b5c3
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-18 18:18:38 +0000

    no message

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 
-$active_app = file_get_contents('addons/roku/roku.php 192.168.1.180 active_app');
+$active_app = exec('php -q addons/roku/roku.php 192.168.1.180 active_app');
 
 print_r($active_app);
