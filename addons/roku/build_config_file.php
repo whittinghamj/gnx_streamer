722d552c2551c4641b717597ffdefc6e1b3a7d1a
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-18 01:20:12 +0000

    no message

--- addons/roku/build_config_file.php
+++ addons/roku/build_config_file.php
@@ -3,8 +3,10 @@
 // build sample config file
 $config[0]['name'] 			= "Demo ROKU #1";
 $config[0]['ip_address'] 	= "192.168.1.180";
+$config[0]['channel'] 		= "sky_cinema_hits";
 $config[1]['name'] 			= "Demo ROKU #2";
 $config[1]['ip_address'] 	= "192.168.1.182";
+$config[1]['channel'] 		= "sky_cinema_comedy";
 
 // convert array to json
 $json = json_encode($config);
