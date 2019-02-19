f8433edbe6a4958727c4b8cc9cc3de0398519c40
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-19 22:02:49 +0000

    no message

--- test.php
+++ test.php
@@ -4,4 +4,15 @@ $raw = shell_exec('/root/speedtest-cli --simple');
 
 $speedtest_bits = explode("\n", $raw);
 
-print_r($speedtest_bits);
+$speedtest_bits = array_filter($speedtest_bits);
+
+$ping = explode(": ", $speedtest_bits[0]);
+$data['ping'] = $ping[1]);
+
+$download = explode(": ", $speedtest_bits[1]);
+$data['download'] = $download[1]);
+
+$upload = explode(": ", $speedtest_bits[2]);
+$data['upload'] = $upload[1]);
+
+print_r($data);
