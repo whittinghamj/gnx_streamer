21f720f2d49dafa048a0e7ca20ccaf5f4d4fadf4
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-19 22:03:28 +0000

    no message

--- test.php
+++ test.php
@@ -7,12 +7,12 @@ $speedtest_bits = explode("\n", $raw);
 $speedtest_bits = array_filter($speedtest_bits);
 
 $ping = explode(": ", $speedtest_bits[0]);
-$data['ping'] = $ping[1]);
+$data['ping'] = $ping[1];
 
 $download = explode(": ", $speedtest_bits[1]);
-$data['download'] = $download[1]);
+$data['download'] = $download[1];
 
 $upload = explode(": ", $speedtest_bits[2]);
-$data['upload'] = $upload[1]);
+$data['upload'] = $upload[1];
 
 print_r($data);
