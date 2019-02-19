8fa931a20b0acc696a504120757a16baeb451754
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-02-19 22:08:25 +0000

    no message

--- console/console.php
+++ console/console.php
@@ -118,3 +118,25 @@ if($task == 'check_dead_hardware') {
 		exec('echo -n "'.$raw.'" | tee /sys/bus/pci/drivers/xhci_hcd/bind');
 	}
 }
+
+if($task == 'speedtest') {
+	console_output("Running speedtest");
+	$raw = shell_exec('/root/speedtest-cli --simple');
+
+	$speedtest_bits = explode("\n", $raw);
+
+	$speedtest_bits = array_filter($speedtest_bits);
+
+	$ping = explode(": ", $speedtest_bits[0]);
+	$data['ping'] = $ping[1];
+
+	$download = explode(": ", $speedtest_bits[1]);
+	$data['download'] = $download[1];
+
+	$upload = explode(": ", $speedtest_bits[2]);
+	$data['upload'] = $upload[1];
+
+	$json = json_decode($data);
+
+	file_put_contents('/var/www/html/config/speedtest.json', $json);
+}
